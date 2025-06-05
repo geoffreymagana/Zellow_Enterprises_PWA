077e05ddcd064b40dee24d77051e3c5b7c241be1
Geoffrey Gitau <geoffreymagana21@gmail.com>
2025-06-05 01:01:41 +0000

    make the main body use the 4 columns instead of only 3.

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -134,7 +134,7 @@ export default function DashboardPage() {
       )}
 
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         {getRoleSpecificItems()}
       </div>
 
