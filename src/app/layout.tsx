13cfca59751806699ad69085d0e452eaf1bef3d7
Geoffrey Gitau <geoffreymagana21@gmail.com>
2025-06-10 17:12:15 +0000

    I LAUNCHED THE APP ONTO VERCEL AND IT WORKS PERFECTLY. THE ONLY ISSUES I

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,14 @@ import { CartProvider } from '@/contexts/CartContext'; // Import CartProvider
 import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
-const manifestBase64 = "eyJuYW1lIjoiWmVsbG93TGl2ZSIsInNob3J0X25hbWUiOiJaZWxsb3dMaXZlIiwiZGVzY3JpcHRpb24iOiJQV0EgZm9yIFplbGxvdyBFbnRlcnByaXNlczogQ3VzdG9tZXIgYW5kIFN0YWZmIFBvcnRhbHMuIiwiaWNvbnMiOlt7InNyYyI6Ii9pY29ucy9pY29uLTE5MngxOTIucG5nIiwic2l6ZXMiOiIxOTJ4MTkyIiwidHlwZSI6ImltYWdlL3BuZyIsInB1cnBvc2UiOiJhbnkgbWFza2FibGUifSx7InNyYyI6Ii9pY29ucy9pY29uLTUxMng1MTIucG5nIiwic2l6ZXMiOiI1MTJ4NTEyIiwidHlwZSI6ImltYWdlL3BuZyJ9XSwic3RhcnRfdXJsIjoiLyIsImRpc3BsYXkiOiJzdGFuZGFsb25lIiwic2NvcGUiOiIvIiwidGhlbWVfY29sb3IiOiIjMzRBN0MxIiwiYmFja2dyb3VuZF9jb2xvciI6IiNGMEY4RkEifQ==";
+// const manifestBase64 = "eyJuYW1lIjoiWmVsbG93TGl2ZSIsInNob3J0X25hbWUiOiJaZWxsb3dMaXZlIiwiZGVzY3JpcHRpb24iOiJQV0EgZm9yIFplbGxvdyBFbnRlcnByaXNlczogQ3VzdG9tZXIgYW5kIFN0YWZmIFBvcnRhbHMuIiwiaWNvbnMiOlt7InNyYyI6Ii9pY29ucy9pY29uLTE5MngxOTIucG5nIiwic2l6ZXMiOiIxOTJ4MTkyIiwidHlwZSI6ImltYWdlL3BuZyIsInB1cnBvc2UiOiJhbnkgbWFza2FibGUifSx7InNyYyI6Ii9pY29ucy9pY29uLTUxMng1MTIucG5nIiwic2l6ZXMiOiI1MTJ4NTEyIiwidHlwZSI6ImltYWdlL3BuZyJ9XSwic3RhcnRfdXJsIjoiLyIsImRpc3BsYXkiOiJzdGFuZGFsb25lIiwic2NvcGUiOiIvIiwidGhlbWVfY29sb3IiOiIjMzRBN0MxIiwiYmFja2dyb3VuZF9jb2xvciI6IiNGMEY4RkEifQ==";
 
 export const metadata: Metadata = {
   title: 'Zellow Enterprises',
   description: 'Zellow Enterprises for Customers and Staff',
-  // manifest: '/manifest.json', // Removed to use inline manifest
+  manifest: '/manifest.json', // Reverted to file-based manifest
   icons: {
-    apple: "/icons/Zellow-icon-192.png", // Basic apple touch icon
+    apple: "/icons/Zellow-icon-192.png",
   },
 };
 
@@ -31,10 +31,10 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         
         {/* PWA Meta Tags */}
-        <meta name="application-name" content="Zellow" />
+        <meta name="application-name" content="ZellowLive" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Zellow" />
+        <meta name="apple-mobile-web-app-title" content="ZellowLive" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -42,14 +42,9 @@ export default function RootLayout({
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#34A7C1" />
 
-        <link rel="manifest" href={`data:application/manifest+json;base64,${manifestBase64}`} />
+        {/* <link rel="manifest" href={`data:application/manifest+json;base64,${manifestBase64}`} /> */}
         
-        {/* More specific apple touch icons (optional, but good practice) */}
         <link rel="apple-touch-icon" href="/icons/Zellow-icon-192.png" />
-        {/* <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" /> */}
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-180x180.png" /> */}
-        {/* <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-167x167.png" /> */}
-
       </head>
       <body className="font-body antialiased" suppressHydrationWarning={true}>
         <ThemeProvider
@@ -59,7 +54,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <AuthProvider>
-            <CartProvider> {/* Wrap with CartProvider */}
+            <CartProvider>
               {children}
               <Toaster />
             </CartProvider>
