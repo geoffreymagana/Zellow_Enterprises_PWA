d97ee5c03447572e6ad205ec4fd29ae5d144da15
Supreme Imprints <62451585+geoffreymagana@users.noreply.github.com>
2025-09-01 17:40:37 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getAnalytics, Analytics } from "firebase/analytics"; // Added
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'pathfinder-ai-eight.vercel.app',
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
